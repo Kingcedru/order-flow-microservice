552ebec26d71dd07ae9ca29f4f2e0478273522cf
kingcedru <byishimocedrick@gmail.com>
2026-06-24 18:22:16 +0200

    feat: add a consumer that listens for orders event

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { OrdersModule } from './orders.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://guest:guest@localhost:5672'],
+      queue: 'orders_queue',
+      queueOptions: { durable: true },
+    },
+  });
+
+  await app.startAllMicroservices();
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();
