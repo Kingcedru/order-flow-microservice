ce1cca16bf74c9924670a13fe1edc9ff2d69d4ce
kingcedru <byishimocedrick@gmail.com>
2026-06-24 18:55:08 +0200

    feat: implement event controller

---
+++ apps/orders/src/order-event.controller.ts
@@ -0,0 +1,12 @@
+import { Controller } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
+import type { OrderCreatedEvent } from '../../../libs/shared/src/events/order-created.event';
+
+@Controller()
+export class OrderEventsController {
+  @EventPattern('order.created')
+  handleOrderCreated(@Payload() data: OrderCreatedEvent) {
+    console.log('📦 Order received:', data);
+    // Next phase: save this to Postgres instead of just logging
+  }
+}
