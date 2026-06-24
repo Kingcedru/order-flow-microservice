cd50e2cc3e38ee122fa6a8a1fc25019732bf85b5
kingcedru <byishimocedrick@gmail.com>
2026-06-24 12:21:02 +0200

    feat: setup rabbitmq in the order service

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'ORDERS_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://guest:guest@localhost:5672'],
+          queue: 'orders_queue',
+          queueOptions: { durable: true },
+        },
+      },
+    ]),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
