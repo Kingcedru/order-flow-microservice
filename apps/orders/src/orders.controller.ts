64d27a0f4a5b477a38600428fb749ffe9d81686d
kingcedru <byishimocedrick@gmail.com>
2026-06-24 18:21:24 +0200

    feat: orders rest endpoint

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { OrderCreatedEvent } from '@app/shared';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller()
 export class OrdersController {
-  constructor(private readonly ordersService: OrdersService) {}
+  constructor(
+    @Inject('ORDERS_SERVICE') private readonly client: ClientProxy,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.ordersService.getHello();
+  @Post()
+  createOrder(@Body() body: { customerId: string; items: any[]; totalAmount: number }) {
+    const event: OrderCreatedEvent = {
+      orderId: crypto.randomUUID(),
+      customerId: body.customerId,
+      items: body.items,
+      totalAmount: body.totalAmount,
+      createdAt: new Date().toISOString(),
+    };
+
+    // emit = fire-and-forget (pub/sub). We'll cover send() (RPC) in Phase 2.
+    this.client.emit('order.created', event);
+
+    return { message: 'Order submitted', orderId: event.orderId };
   }
 }
